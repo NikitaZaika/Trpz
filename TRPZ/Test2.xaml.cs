4009118acc586bfd421e6c961559890239a1549d
NikitaZaika <zaika.nikita.01@gmail.com>
2020-02-17 02:51:29 +0200

    добавил окно

---
+++ TRPZ/Test2.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TRPZ
+{
+    /// <summary>
+    /// Логика взаимодействия для Test2.xaml
+    /// </summary>
+    public partial class Test2 : Window
+    {
+        public Test2()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TRPZ/Test_class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TRPZ
+{
+    class Test_class1
+    {
+        //111111
+    }
+}
